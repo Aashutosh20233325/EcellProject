b9ee6b3e780e422a765858b294a79b229656f970
Aashutosh20233325 <fitjeeaim6837@gmail.com>
2025-01-22 15:09:57 +0530

    Added Navbar Component

--- ecelltask/src/App.js
+++ ecelltask/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-
+import Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './Components/Home/Home'
+import Grid from './Components/Grid/Grid'
+import Linkdin from './Components/Linkdin/Linkdin'
 function App() {
   return (
-    <div className="App">
-     Hello
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element = {<Grid/>}/>
+        <Route path='/home' element ={<Home/>}/>
+        <Route path='/linkdin' element ={<Linkdin/>}/>
+      </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ ecelltask/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React , {useState , useRef} from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom';
+const Navbar = () => {
+
+    const[menu,setmenu]=useState("grid");
+    const menuRef= useRef();
+  return (
+    
+    <div className ='navbar'>
+       <ul ref={menuRef} className="nav-menu">
+            <li onClick={()=>{setmenu("home")}}><Link style={{textDecoration:'none'}}to='/home'>Home</Link>{menu==="home"?<hr/>:<></>}</li>
+            <li onClick={()=>{setmenu("grid")}}><Link style={{textDecoration:'none'}}to='/'>Grid</Link>{menu==="grid"?<hr/>:<></>}</li>
+            <li onClick={()=>{setmenu("linkdin")}}><Link style={{textDecoration:'none'}}to='/Linkdin'>Linkdin</Link>{menu==="linkdin"?<hr/>:<></>}</li>
+
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
